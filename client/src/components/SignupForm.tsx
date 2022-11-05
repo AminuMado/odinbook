8a068802c9dd961415bb0ba54120695f275621f9
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-11-05 21:53:26 +0100

    add signup form logidc

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,3 +1,55 @@
+import "./SignupForm.css";
+import { useState } from "react";
+
 export const SignupForm = () => {
-  return <div>Hello Signup Form</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+
+  return (
+    <div className="signup-container">
+      <form className="form">
+        <h1>SIGN UP</h1>
+        <input
+          type="text"
+          placeholder="First Name"
+          className="form__input"
+          onChange={(e) => setFirstname(e.target.value)}
+          value={firstname}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          className="form__input"
+          onChange={(e) => setLastname(e.target.value)}
+          value={lastname}
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          className="form__input"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="form__input"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form__input"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button className="btn">Sign Up</button>
+      </form>
+      <p>Already have an account? Log In</p>
+    </div>
+  );
 };
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import { LoginForm } from "../components/loginForm";
+import { SignupForm } from "../components/SignupForm";
 import "./Landing.css";
 export const Landing = () => {
+  const [isLogin, setIsLogin] = useState(true);
   return (
     <div className="Landing">
       <h1 className="Landing__title">The Cafe</h1>
+      <div className="Landing__testaccount-container">
+        <h1 className="Landing__testaccount">Just looking around?</h1>
+        <button className="Landing__testaccountBtn">Test Account</button>
+      </div>
+      <div>
+        <button>Login</button>
+        <button>Signup</button>
+      </div>
     </div>
   );
 };
