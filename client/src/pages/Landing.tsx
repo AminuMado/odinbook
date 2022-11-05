779594a3b74de5e8db4fda4a19b5820823dcd051
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-11-05 22:44:39 +0100

    add modal

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import { LoginForm } from "../components/LoginForm";
+import { Modal } from "../components/Modal";
 import { SignupForm } from "../components/SignupForm";
 import "./Landing.css";
 export const Landing = () => {
   const [isLogin, setIsLogin] = useState(true);
   return (
-    <div className="Landing">
-      <h1 className="Landing__title">The Cafe</h1>
-      <div className="Landing__testaccount-container">
-        <h1 className="Landing__testaccount">Just looking around?</h1>
-        <button className="Landing__testaccountBtn">Test Account</button>
+    <>
+      <Modal />
+      <div className="Landing">
+        <h1 className="Landing__title">The Cafe</h1>
+        <div className="Landing__btn-container">
+          <button className="login-btn">LOGIN</button>
+          <button className="signup-btn">SIGNUP</button>
+        </div>
+        <div className="Landing__testaccount-container">
+          <h1 className="Landing__testaccount">Just looking around?</h1>
+          <button className="Landing__testaccountBtn">Test Account</button>
+        </div>
       </div>
-      <div>
-        <button>Login</button>
-        <button>Signup</button>
-      </div>
-    </div>
+    </>
   );
 };
