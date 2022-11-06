37862d0867d3399e5e57ff2c3999440ff0681512
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-11-06 14:13:58 +0100

    add signup and login form modal logic

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,24 +1,45 @@
 import { useState } from "react";
 import { LoginForm } from "../components/LoginForm";
-import { Modal } from "../components/Modal";
 import { SignupForm } from "../components/SignupForm";
 import "./Landing.css";
 export const Landing = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+  const clearAll = () => {
+    setIsLogin(false);
+    setIsSignup(false);
+  };
   return (
-    <>
-      <Modal />
-      <div className="Landing">
-        <h1 className="Landing__title">The Cafe</h1>
-        <div className="Landing__btn-container">
-          <button className="login-btn">LOGIN</button>
-          <button className="signup-btn">SIGNUP</button>
-        </div>
-        <div className="Landing__testaccount-container">
-          <h1 className="Landing__testaccount">Just looking around?</h1>
-          <button className="Landing__testaccountBtn">Test Account</button>
-        </div>
+    <div className="Landing">
+      <h1 className="Landing__title">The Cafe</h1>
+      <div className="Landing__testaccount-container">
+        <h1 className="Landing__testaccount">Just looking around?</h1>
+        <button className="Landing__testaccountBtn">Test Account</button>
       </div>
-    </>
+      <div className="Landing__btn-container">
+        <button
+          onClick={() => {
+            clearAll();
+            setIsLogin(!isLogin);
+          }}
+          className="login-btn"
+        >
+          LOGIN
+        </button>
+        <button
+          onClick={() => {
+            clearAll();
+            setIsSignup(!isSignup);
+          }}
+          className="signup-btn"
+        >
+          SIGNUP
+        </button>
+      </div>
+      <div>
+        {isLogin && <LoginForm />}
+        {isSignup && <SignupForm />}
+      </div>
+    </div>
   );
 };
