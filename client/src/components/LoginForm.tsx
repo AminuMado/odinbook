60b0e8ba75fb1c0a5e84c294ab48cdf66f0243d7
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-11-15 21:41:31 +0100

    import useLogin hook and loader

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./LoginForm.css";
+import { useLogin } from "../hooks/useLogin";
+import { Loader } from "./Loader";
 export const LoginForm = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const { login, isLoading, error } = useLogin();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login(username, password);
+  };
   return (
     <div className="login-container">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h1>LOG IN</h1>
         <input
           type="email"
@@ -21,8 +28,11 @@ export const LoginForm = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
-        <button className="btn">Log In</button>
+        {isLoading ? <Loader /> : <button className="btn">Log In</button>}
       </form>
+      <span data-testid="error" className="error-msg">
+        {error}
+      </span>
     </div>
   );
 };
