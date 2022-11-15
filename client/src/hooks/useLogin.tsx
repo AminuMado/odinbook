dcf9ca6a565f9dd4e0b0656858a6f6747e271e8c
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-11-15 21:40:18 +0100

    add fakejsonplaceholder api url

--- client/src/hooks/useLogin.tsx
+++ client/src/hooks/useLogin.tsx
@@ -3,16 +3,12 @@ import { useState } from "react";
 export const useLogin = () => {
   const [error, setError] = useState<null | string>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const url = "a test url";
+  const randomSeed = Math.floor(Math.random() * 11); // for testing need to change later
+  const url = `https://jsonplaceholder.typicode.com/users/${randomSeed}`;
   const login = async (email: string, password: string) => {
     setIsLoading(true);
     setError(null);
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+    const response = await fetch(url);
     const json = await response.json();
     if (!response.ok) {
       setIsLoading(false);
@@ -28,3 +24,11 @@ export const useLogin = () => {
   };
   return { login, isLoading, error };
 };
+
+/**
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    */
