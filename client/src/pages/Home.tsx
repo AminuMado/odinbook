8e74fd4a824c929635568aa61562b8ba20e1aa69
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-11-20 05:59:25 +0100

    add Todos comment

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,9 @@
 export const Home = () => {
+  /**
+   * Todos
+   * should have a navbar
+   * should have the posts card
+   * should have the create post input
+   */
   return <div>Hello Home</div>;
 };
