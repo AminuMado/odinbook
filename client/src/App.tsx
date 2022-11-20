e46b1b2d70f87d97dc21dc7053aad29cdbd6e638
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-11-20 06:00:10 +0100

    add protected routes logic to compoenent by using the useAuthcontext and validating if a user is logged in or not

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,20 @@
 import "./App.css";
 import { Landing } from "./pages/Landing";
-import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home";
-
+import { useAuthContext } from "./hooks/useAuthContext";
 function App() {
+  const { state } = useAuthContext();
+  const user = state.user;
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
+          <Route
+            path="/"
+            element={!user ? <Landing /> : <Navigate to="/home" />}
+          />
+          <Route path="/home" element={user ? <Home /> : <Navigate to="/" />} />
         </Routes>
       </div>
     </BrowserRouter>
