0c6815e6218140450fd66eaaf44196a7b36b55b7
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-02 04:25:14 +0100

    import PostCard

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -1,3 +1,4 @@
+import { PostCard } from "../Card/PostCard";
 import { NewPost } from "./NewPost";
 
 export const Post = () => {
@@ -5,6 +6,7 @@ export const Post = () => {
     <div>
       {/* This should have the new post and the posts in them the posts this should be where the api call occurs so id need to import a post card also */}
       <NewPost />
+      <PostCard />
     </div>
   );
 };
