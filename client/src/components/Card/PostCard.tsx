3f6ac23059320e84cd3a7c81cbb8a13dbbe31b4f
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-08 14:46:39 +0100

    update the layout and logic behind the collapsible component

--- client/src/components/Card/PostCard.tsx
+++ client/src/components/Card/PostCard.tsx
@@ -1,8 +1,5 @@
 import "./PostCard.css";
 import avatar_1 from "../../Assets/Images/1.jpg";
-import avatar_2 from "../../Assets/Images/2.jpg";
-import avatar_3 from "../../Assets/Images/3.jpg";
-import avatar_4 from "../../Assets/Images/4.jpg";
 import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
 import { NewComment } from "../Comment/CommentForm";
 import { useState } from "react";
@@ -10,6 +7,8 @@ import { Collapsible } from "../Collapsible/Collapsible";
 import { Comment } from "../Comment/Comment";
 export const PostCard = () => {
   const [showNewComment, setShowNewComment] = useState(false);
+  const [showComments, setShowComments] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
   const likeCount = 0;
   const commentCount = 1;
   return (
@@ -35,174 +34,52 @@ export const PostCard = () => {
             Schofield and i am awesomecHello I am Michael Schofield awesome.
           </p>
           <div className="postCard-body__details">
-            {/* i wanna have a top and bottom 
-            
-            top containst the like and comment buttons
-            the bottom contains the comments. So a button push activates the collapsible*/}
-            {likeCount ? (
-              <button className="postCard-options__likes">
-                {`${likeCount} like${commentCount > 1 ? "s" : ""}`}
-              </button>
-            ) : (
-              <p>0 Likes</p>
-            )}
-            {commentCount ? (
-              <Collapsible
-                label={`${commentCount} comment${commentCount > 1 ? "s" : ""}`}
-                clicked={() => {}}
-                styleName="postCard-options__comments"
-              >
-                <>
-                  <Comment />
-                  <Comment />
-                  <Comment />
-                  <Comment />
-                </>
-              </Collapsible>
-            ) : (
-              <p>0 Comments</p>
-            )}
+            <div className="postCard-body__details-top">
+              {likeCount ? (
+                <p className="postCard-options__likes">
+                  {`${likeCount} like${likeCount > 1 ? "s" : ""}`}
+                </p>
+              ) : (
+                <p>0 Likes</p>
+              )}
+              {commentCount ? (
+                <p
+                  className="postCard-options__comments"
+                  onClick={() => setShowComments(!showComments)}
+                >
+                  {`${commentCount} like${commentCount > 1 ? "s" : ""}`}
+                </p>
+              ) : (
+                <p>0 Comments</p>
+              )}
+            </div>
+          </div>
+          <div className="postCard-body__details-bottom">
+            <Collapsible isOpen={showComments}>
+              <>
+                <Comment />
+                <Comment />
+                <Comment />
+              </>
+            </Collapsible>
           </div>
         </div>
         <div className="postCard-options">
           <button
+            onClick={() => setIsLiked(!isLiked)}
             className={`postCard-options__like ${showNewComment ? "show" : ""}`}
           >
-            Like
+            {isLiked ? "Unlike" : "Like"}
           </button>
-          <Collapsible
-            label="Comment"
-            clicked={setShowNewComment}
-            styleName={`postCard-options__comment ${
-              showNewComment ? "show" : ""
-            }`}
-          >
-            <NewComment />
-          </Collapsible>
-        </div>
-      </div>
-      <div className="postCard-container">
-        <div className="postCard-header">
-          <div className="postCard-header__left">
-            <img
-              className="postCard-header__userAvatar"
-              src={avatar_2}
-              alt="avatar"
-            />
-            <p className="postCard-header__username">Mahatma Ghandi</p>
-            <p className="postCard-header__datePosted">posted 2/12/2022</p>
-          </div>
-          <div className="postCard-header__right">
-            <DeleteOutlineRoundedIcon className="postCard-header__deleteIcon" />
-          </div>
-        </div>
-        <div className="postCard-body">
-          <p className="postCard-body__content">
-            Hello I am Michael Schofield and i am awesome Hello I am Michael
-            Schofield and i am awesomecHello I am Michael Schofield awesome.
-          </p>
-          <div className="postCard-body__details">
-            <p>Likes</p>
-            <p>Comments</p>
-          </div>
-        </div>
-        <div className="postCard-options">
           <button
-            className={`postCard-options__like ${showNewComment ? "show" : ""}`}
-          >
-            Like
-          </button>
-          <Collapsible
-            label="Comment"
-            clicked={setShowNewComment}
-            styleName={`postCard-options__comment ${
+            onClick={() => setShowNewComment(!showNewComment)}
+            className={`postCard-options__comment ${
               showNewComment ? "show" : ""
             }`}
           >
-            <NewComment />
-          </Collapsible>
-        </div>
-      </div>
-      <div className="postCard-container">
-        <div className="postCard-header">
-          <div className="postCard-header__left">
-            <img
-              className="postCard-header__userAvatar"
-              src={avatar_3}
-              alt="avatar"
-            />
-            <p className="postCard-header__username">Sister May</p>
-            <p className="postCard-header__datePosted">posted 2/12/2022</p>
-          </div>
-          <div className="postCard-header__right">
-            <DeleteOutlineRoundedIcon className="postCard-header__deleteIcon" />
-          </div>
-        </div>
-        <div className="postCard-body">
-          <p className="postCard-body__content">
-            Hello I am Michael Schofield and i am awesome Hello I am Michael
-            Schofield and i am awesomecHello I am Michael Schofield awesome.
-          </p>
-          <div className="postCard-body__details">
-            <p>Likes</p>
-            <p>Comments</p>
-          </div>
-        </div>
-        <div className="postCard-options">
-          <button
-            className={`postCard-options__like ${showNewComment ? "show" : ""}`}
-          >
-            Like
+            Comment
           </button>
-          <Collapsible
-            label="Comment"
-            clicked={setShowNewComment}
-            styleName={`postCard-options__comment ${
-              showNewComment ? "show" : ""
-            }`}
-          >
-            <NewComment />
-          </Collapsible>
-        </div>
-      </div>
-      <div className="postCard-container">
-        <div className="postCard-header">
-          <div className="postCard-header__left">
-            <img
-              className="postCard-header__userAvatar"
-              src={avatar_4}
-              alt="avatar"
-            />
-            <p className="postCard-header__username">White Man</p>
-            <p className="postCard-header__datePosted">posted 2/12/2022</p>
-          </div>
-          <div className="postCard-header__right">
-            <DeleteOutlineRoundedIcon className="postCard-header__deleteIcon" />
-          </div>
-        </div>
-        <div className="postCard-body">
-          <p className="postCard-body__content">
-            Hello I am Michael Schofield and i am awesome Hello I am Michael
-            Schofield and i am awesomecHello I am Michael Schofield awesome.
-          </p>
-          <div className="postCard-body__details">
-            <p>Likes</p>
-            <p>Comments</p>
-          </div>
-        </div>
-        <div className="postCard-options">
-          <button
-            className={`postCard-options__like ${showNewComment ? "show" : ""}`}
-          >
-            Like
-          </button>
-          <Collapsible
-            label="Comment"
-            clicked={setShowNewComment}
-            styleName={`postCard-options__comment ${
-              showNewComment ? "show" : ""
-            }`}
-          >
+          <Collapsible isOpen={showNewComment}>
             <NewComment />
           </Collapsible>
         </div>
