9ef002d350cfabd4b3acc34c7a02e8293427fae5
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-08 14:47:14 +0100

    update the component logic to use props to handle state

--- client/src/components/Collapsible/Collapsible.tsx
+++ client/src/components/Collapsible/Collapsible.tsx
@@ -1,29 +1,18 @@
 import "./Collapsible.css";
-import { useState, useRef } from "react";
+import { useRef } from "react";
 type props = {
-  label: string;
-  styleName: string;
+  isOpen: boolean;
   children: JSX.Element;
-  clicked: React.Dispatch<React.SetStateAction<boolean>>;
 };
-export const Collapsible = ({ label, styleName, clicked, children }: props) => {
-  const [open, setOpen] = useState(false);
-  const toggle = () => {
-    clicked((prev) => !prev);
-    setOpen(!open);
-  };
+export const Collapsible = ({ isOpen, children }: props) => {
   const contentRef = useRef<HTMLDivElement>(null);
-  if (contentRef.current) console.log(contentRef.current.scrollHeight);
   return (
     <>
-      <button className={styleName} onClick={toggle}>
-        {label}
-      </button>
       <div
         ref={contentRef}
         className="content-parent"
         style={
-          open
+          isOpen
             ? { height: contentRef.current?.scrollHeight + "px" }
             : { height: "0px" }
         }
