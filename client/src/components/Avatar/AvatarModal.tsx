21f7daf8659b251e5a820dc3ee34a1417925feaf
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-13 00:20:07 +0100

    add props to make component dynamic

--- client/src/components/Avatar/AvatarModal.tsx
+++ client/src/components/Avatar/AvatarModal.tsx
@@ -1,11 +1,17 @@
 import "./AvatarModal.css";
-import avatar1 from "../../Assets/Images/1.jpg";
-export const AvatarModal = () => {
+type props = {
+  toggle: React.Dispatch<React.SetStateAction<boolean>>;
+  avatar: string;
+};
+export const AvatarModal = ({ toggle, avatar }: props) => {
   // This component should have a modal and the image container
   return (
     <div className="avatarModal">
-      <div className="avatarModal__overlay" />
-      <img src={avatar1} alt="avatar" className="avatarModal__avatar" />
+      <div
+        className="avatarModal__overlay"
+        onClick={() => toggle((prev) => !prev)}
+      />
+      <img src={avatar} alt="avatar" className="avatarModal__avatar" />
     </div>
   );
 };
