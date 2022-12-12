5b188cf9c9068c46d0615549f23ed4caf7dbfde8
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-13 00:18:22 +0100

    add props and made component dynamic

--- client/src/components/Comment/Comment.tsx
+++ client/src/components/Comment/Comment.tsx
@@ -1,30 +1,47 @@
 import "./Comment.css";
-import avatar_1 from "../../Assets/Images/1.jpg";
 import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
-
-export const Comment = () => {
+import { AvatarModal } from "../Avatar/AvatarModal";
+import { useState } from "react";
+type props = {
+  avatar: string;
+  username: string;
+  datePosted: string;
+  content: string;
+};
+export const Comment = ({ avatar, content, datePosted, username }: props) => {
+  const [showAvatarModal, setShowAvatarModal] = useState(false);
   return (
-    <div className="comment-container">
-      {/* should have certain things
+    <>
+      {showAvatarModal && (
+        <AvatarModal toggle={setShowAvatarModal} avatar={avatar} />
+      )}
+      <div className="comment-container">
+        {/* should have certain things
             1. Should have an avatar.
             2. should have a username
             3. Should have a date posted
             4. Should have a delete button
             5. Should have a content
-        */}
-      <img className="comment__userAvatar" src={avatar_1} alt="avatar" />
-      <div className="comment-content-container">
-        <div className="comment-header-container">
-          <div className="comment-header-left">
-            <p className="comment-header__username"> Micheal Scofield</p>
-            <p className="comment-header__datePosted">posted 2/12/2022</p>
+          */}
+        <img
+          className="comment__userAvatar"
+          src={avatar}
+          alt="avatar"
+          onClick={() => setShowAvatarModal(!showAvatarModal)}
+        />
+        <div className="comment-content-container">
+          <div className="comment-header-container">
+            <div className="comment-header-left">
+              <p className="comment-header__username"> {username}</p>
+              <p className="comment-header__datePosted">posted {datePosted}</p>
+            </div>
+            <div className="comment-header-right">
+              <DeleteOutlineRoundedIcon className="comment-header__deleteIcon" />
+            </div>
           </div>
-          <div className="comment-header-right">
-            <DeleteOutlineRoundedIcon className="comment-header__deleteIcon" />
-          </div>
+          <p className="comment-content-text">{content}</p>
         </div>
-        <p className="comment-content-text">Hello this is an example comment</p>
       </div>
-    </div>
+    </>
   );
 };
