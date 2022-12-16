268e90ba77f80317b4ef293395430763691b1840
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-16 18:48:36 +0100

    add nav routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,27 @@ import { Landing } from "./pages/Landing";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { useAuthContext } from "./hooks/useAuthContext";
+import { Profile } from "./pages/Profile";
+import { Navbar } from "./components/Navbar/Navbar";
 function App() {
   const { state } = useAuthContext();
   const user = state.user;
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
+
         <Routes>
           <Route
             path="/"
             element={!user ? <Landing /> : <Navigate to="/home" />}
           />
+
           <Route path="/home" element={user ? <Home /> : <Navigate to="/" />} />
+          <Route
+            path="/profile"
+            element={user ? <Profile /> : <Navigate to="/" />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
