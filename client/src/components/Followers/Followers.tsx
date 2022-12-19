4cbfed590f06975c005f118c404f7865e315bded
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-19 11:06:01 +0100

    initial commit

---
+++ client/src/components/Followers/Followers.tsx
@@ -0,0 +1,84 @@
+import "./Followers.css";
+import avatar1 from "../../Assets/Images/1.jpg";
+import avatar2 from "../../Assets/Images/2.jpg";
+import avatar3 from "../../Assets/Images/3.jpg";
+import avatar4 from "../../Assets/Images/24.jpg";
+import avatar5 from "../../Assets/Images/25.jpg";
+export const Followers = () => {
+  return (
+    <div className="followers-container">
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar1} alt="avatar" />
+        <p>Micheal Schofield</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
+        <p>Lady Random</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar3} alt="avatar" />
+        <p>Elden Lord</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar4} alt="avatar" />
+        <p>Magnussen</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar5} alt="avatar" />
+        <p>Nakamura</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
+        <p>Palo Alto</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar1} alt="avatar" />
+        <p>Micheal Schofield</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
+        <p>Lady Random</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar3} alt="avatar" />
+        <p>Elden Lord</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar4} alt="avatar" />
+        <p>Magnussen</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar5} alt="avatar" />
+        <p>Nakamura</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
+        <p>Palo Alto</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar1} alt="avatar" />
+        <p>Micheal Schofield</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
+        <p>Lady Random</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar3} alt="avatar" />
+        <p>Elden Lord</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar4} alt="avatar" />
+        <p>Magnussen</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar5} alt="avatar" />
+        <p>Nakamura</p>
+      </div>
+      <div className="follower__card">
+        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
+        <p>Palo Alto</p>
+      </div>
+    </div>
+  );
+};
