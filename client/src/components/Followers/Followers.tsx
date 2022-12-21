3ee81bc7e9530f1fb11415f541eee61905086f7d
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-21 22:56:30 +0100

    add follow button

--- client/src/components/Followers/Followers.tsx
+++ client/src/components/Followers/Followers.tsx
@@ -10,74 +10,59 @@ export const Followers = () => {
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar1} alt="avatar" />
         <p>Micheal Schofield</p>
+        <button>Unfollow</button>
       </div>
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar2} alt="avatar" />
         <p>Lady Random</p>
+        <button>Unfollow</button>
       </div>
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar3} alt="avatar" />
         <p>Elden Lord</p>
+        <button>Unfollow</button>
       </div>
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar4} alt="avatar" />
-        <p>Magnussen</p>
+        <p>Magnussen and a lot of </p>
+        <button>Unfollow</button>
       </div>
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar5} alt="avatar" />
         <p>Nakamura</p>
+        <button>Follow</button>
       </div>
+
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar2} alt="avatar" />
         <p>Palo Alto</p>
+        <button>Unfollow</button>
       </div>
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar1} alt="avatar" />
         <p>Micheal Schofield</p>
+        <button>Follow</button>
       </div>
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar2} alt="avatar" />
         <p>Lady Random</p>
+        <button>Follow</button>
       </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar3} alt="avatar" />
-        <p>Elden Lord</p>
-      </div>
+
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar4} alt="avatar" />
         <p>Magnussen</p>
+        <button>Follow</button>
       </div>
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar5} alt="avatar" />
         <p>Nakamura</p>
+        <button>Follow</button>
       </div>
       <div className="follower__card">
         <img className="follower__card_avatar" src={avatar2} alt="avatar" />
         <p>Palo Alto</p>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar1} alt="avatar" />
-        <p>Micheal Schofield</p>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
-        <p>Lady Random</p>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar3} alt="avatar" />
-        <p>Elden Lord</p>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar4} alt="avatar" />
-        <p>Magnussen</p>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar5} alt="avatar" />
-        <p>Nakamura</p>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
-        <p>Palo Alto</p>
+        <button>Follow</button>
       </div>
     </div>
   );
