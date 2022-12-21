60d0fb96f0bd01e671ede547c31587ab2478e58f
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-21 22:55:04 +0100

    initial commit

---
+++ client/src/testUser.tsx
@@ -0,0 +1,61 @@
+import avatar1 from "./Assets/Images/6.jpg";
+import avatar2 from "./Assets/Images/16.jpg";
+import avatar3 from "./Assets/Images/7.jpg";
+import avatar4 from "./Assets/Images/17.jpg";
+import avatar5 from "./Assets/Images/8.jpg";
+import avatar6 from "./Assets/Images/18.jpg";
+import avatar7 from "./Assets/Images/9.jpg";
+import avatar8 from "./Assets/Images/19.jpg";
+import avatar9 from "./Assets/Images/10.jpg";
+import avatar10 from "./Assets/Images/20.jpg";
+
+export const testUser1 = {
+  userId: "test1",
+  username: "Tchalla",
+  avatar: avatar1,
+};
+export const testUser2 = {
+  userId: "test2",
+  username: "Rainbow",
+  avatar: avatar2,
+};
+export const testUser3 = {
+  userId: "test3",
+  username: "Cowboy",
+  avatar: avatar3,
+};
+export const testUser4 = {
+  userId: "test4",
+  username: "Beardy",
+  avatar: avatar4,
+};
+export const testUser5 = {
+  userId: "test5",
+  username: "Glasses",
+  avatar: avatar5,
+};
+export const testUser6 = {
+  userId: "test6",
+  username: "Bianca",
+  avatar: avatar6,
+};
+export const testUser7 = {
+  userId: "test7",
+  username: "Unibrow",
+  avatar: avatar7,
+};
+export const testUser8 = {
+  userId: "test8",
+  username: "Mustache",
+  avatar: avatar8,
+};
+export const testUser9 = {
+  userId: "test9",
+  username: "Turban",
+  avatar: avatar9,
+};
+export const testUser10 = {
+  userId: "test10",
+  username: "Admin",
+  avatar: avatar10,
+};
