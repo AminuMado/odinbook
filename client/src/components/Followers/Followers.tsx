092a3920a2fa0182879537be3eb629ea02d1be03
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-25 23:14:05 +0100

    add FollowerCard

--- client/src/components/Followers/Followers.tsx
+++ client/src/components/Followers/Followers.tsx
@@ -4,66 +4,77 @@ import avatar2 from "../../Assets/Images/2.jpg";
 import avatar3 from "../../Assets/Images/3.jpg";
 import avatar4 from "../../Assets/Images/24.jpg";
 import avatar5 from "../../Assets/Images/25.jpg";
+import { FollowerCard } from "../Following/FollowerCard";
 export const Followers = () => {
+  const sampleFollowers = [
+    {
+      userId: "test1",
+      username: "Tchalla Black Panther",
+      avatar: avatar1,
+    },
+    {
+      userId: "test2",
+      username: "Random Lady",
+      avatar: avatar2,
+    },
+    {
+      userId: "test3",
+      username: "Elden Lord",
+      avatar: avatar3,
+    },
+    {
+      userId: "test41",
+      username: "Magnus Carlsen",
+      avatar: avatar4,
+    },
+    {
+      userId: "test2",
+      username: "Random Lady",
+      avatar: avatar2,
+    },
+    {
+      userId: "test51",
+      username: "Pablo Escobar",
+      avatar: avatar5,
+    },
+    {
+      userId: "test41",
+      username: "Magnus Carlsen",
+      avatar: avatar4,
+    },
+    {
+      userId: "test1",
+      username: "Tchalla Black Panther",
+      avatar: avatar1,
+    },
+    {
+      userId: "test3",
+      username: "Elden Lord",
+      avatar: avatar3,
+    },
+    {
+      userId: "test41",
+      username: "Magnus Carlsen",
+      avatar: avatar4,
+    },
+    {
+      userId: "test51",
+      username: "Pablo Escobar",
+      avatar: avatar5,
+    },
+  ];
   return (
     <div className="followers-container">
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar1} alt="avatar" />
-        <p>Micheal Schofield</p>
-        <button>Unfollow</button>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
-        <p>Lady Random</p>
-        <button>Unfollow</button>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar3} alt="avatar" />
-        <p>Elden Lord</p>
-        <button>Unfollow</button>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar4} alt="avatar" />
-        <p>Magnussen and a lot of </p>
-        <button>Unfollow</button>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar5} alt="avatar" />
-        <p>Nakamura</p>
-        <button>Follow</button>
-      </div>
-
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
-        <p>Palo Alto</p>
-        <button>Unfollow</button>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar1} alt="avatar" />
-        <p>Micheal Schofield</p>
-        <button>Follow</button>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
-        <p>Lady Random</p>
-        <button>Follow</button>
-      </div>
-
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar4} alt="avatar" />
-        <p>Magnussen</p>
-        <button>Follow</button>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar5} alt="avatar" />
-        <p>Nakamura</p>
-        <button>Follow</button>
-      </div>
-      <div className="follower__card">
-        <img className="follower__card_avatar" src={avatar2} alt="avatar" />
-        <p>Palo Alto</p>
-        <button>Follow</button>
-      </div>
+      {sampleFollowers.map((follower) => {
+        return (
+          <FollowerCard
+            id={follower.userId}
+            key={follower.userId}
+            username={follower.username}
+            avatar={follower.avatar}
+          />
+        );
+      })}
     </div>
   );
 };
