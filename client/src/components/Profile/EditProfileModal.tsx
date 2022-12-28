2182054f60205d67b1d54b1dc3d40522bf4e4650
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-28 10:27:01 +0100

    add state values for input commands

--- client/src/components/Profile/EditProfileModal.tsx
+++ client/src/components/Profile/EditProfileModal.tsx
@@ -1,31 +1,46 @@
+import { useState } from "react";
+import { useUserContext } from "../../hooks/useUserContext";
 import "./EditProfileModal.css";
-type props = {
-  toggle: React.Dispatch<React.SetStateAction<boolean>>;
-  avatar: string;
-};
-export const EditProfileModal = ({ toggle, avatar }: props) => {
+
+// type props = {
+//   toggle: React.Dispatch<React.SetStateAction<boolean>>;
+//   avatar: string;
+// };
+export const EditProfileModal = () => {
+  const { state } = useUserContext();
+  const [username, setUsername] = useState(state.user.username);
+  const [about, setAbout] = useState(state.user.about);
+  const [avatar, setAvatar] = useState(state.user.avatar);
   // This component should have the capability to edit the profile, so chaning your profile picture and username and about me
   return (
     <div className="editProfileModal">
       <div
         className="editProfileModal__overlay"
-        onClick={() => toggle((prev) => !prev)}
+        // onClick={() => toggle((prev) => !prev)}
       />
       {/* So we need an input for username,about me and profile photo, at the end it should be preloaded with the current user's detials from the context */}
       <div className="editProfileModalForm-container">
+        <img className="editProfileModal__avatar" src={avatar} alt="avatar" />
         <form>
+          <button type="button" id="avatar">
+            Change Avatar
+          </button>
           <label htmlFor="username">Username</label>
           <input
             className="editProfileModalForm__input"
             type="text"
             id="username"
             name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <label htmlFor="about">About Me</label>
           <textarea
             className="editProfileModal__textarea"
             id="about"
             name="about"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
           />
           <button className="editProfileModal__button" type="submit">
             Submit
