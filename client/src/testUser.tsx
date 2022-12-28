cc7636ea9324ff93fdbae5a71a9cf2bae2a3bbaf
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-28 10:25:59 +0100

    add about field

--- client/src/testUser.tsx
+++ client/src/testUser.tsx
@@ -11,51 +11,71 @@ import avatar10 from "./Assets/Images/20.jpg";
 
 export const testUser1 = {
   userId: "test1",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Tchalla",
   avatar: avatar1,
 };
 export const testUser2 = {
   userId: "test2",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Rainbow",
   avatar: avatar2,
 };
 export const testUser3 = {
   userId: "test3",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Cowboy",
   avatar: avatar3,
 };
 export const testUser4 = {
   userId: "test4",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Beardy",
   avatar: avatar4,
 };
 export const testUser5 = {
   userId: "test5",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Glasses",
   avatar: avatar5,
 };
 export const testUser6 = {
   userId: "test6",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Bianca",
   avatar: avatar6,
 };
 export const testUser7 = {
   userId: "test7",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Unibrow",
   avatar: avatar7,
 };
 export const testUser8 = {
   userId: "test8",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Mustache",
   avatar: avatar8,
 };
 export const testUser9 = {
   userId: "test9",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Turban",
   avatar: avatar9,
 };
 export const testUser10 = {
   userId: "test10",
+  about:
+    "This is supposed to be the about, just fill in the words for testing, kind of like a lorem ipsum",
   username: "Admin",
   avatar: avatar10,
 };
