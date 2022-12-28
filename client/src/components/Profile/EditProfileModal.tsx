2ff07cd9c51afa7324b7c14ee53ed70011b78c99
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-28 15:55:37 +0100

    add toggle function to switch between active and inactive states

--- client/src/components/Profile/EditProfileModal.tsx
+++ client/src/components/Profile/EditProfileModal.tsx
@@ -2,23 +2,21 @@ import { useState } from "react";
 import { useUserContext } from "../../hooks/useUserContext";
 import "./EditProfileModal.css";
 
-// type props = {
-//   toggle: React.Dispatch<React.SetStateAction<boolean>>;
-//   avatar: string;
-// };
-export const EditProfileModal = () => {
+type props = {
+  toggle: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export const EditProfileModal = ({ toggle }: props) => {
   const { state } = useUserContext();
   const [username, setUsername] = useState(state.user.username);
   const [about, setAbout] = useState(state.user.about);
   const [file, setFile] = useState<Blob | MediaSource | null>(null);
-
   const [avatar, setAvatar] = useState(state.user.avatar);
   // This component should have the capability to edit the profile, so chaning your profile picture and username and about me
   return (
     <div className="editProfileModal">
       <div
         className="editProfileModal__overlay"
-        // onClick={() => toggle((prev) => !prev)}
+        onClick={() => toggle((prev) => !prev)}
       />
       {/* So we need an input for username,about me and profile photo, at the end it should be preloaded with the current user's detials from the context */}
       <div className="editProfileModalForm-container">
