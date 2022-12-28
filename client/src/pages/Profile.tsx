e36b547eda8e5dc74a02470f6d14bf704374fa98
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-28 10:27:51 +0100

    import EditprofileModal

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -5,46 +5,50 @@ import { useState } from "react";
 import { Followers } from "../components/Followers/Followers";
 import { Following } from "../components/Following/Following";
 import ManageAccountsTwoToneIcon from "@mui/icons-material/ManageAccountsTwoTone";
+import { EditProfileModal } from "../components/Profile/EditProfileModal";
 export const Profile = () => {
   const [activeTab, setActiveTab] = useState("Posts");
   const tabs = ["Posts", "Followers", "Following"];
   return (
-    <div className="profile">
-      <div className="profile__userInfo-container">
-        <img src={avatar} alt="avatar" className="profile__userInfo_avatar" />
-        <div className="profile__userInfo">
-          <div className="profile__userInfo_top">
-            <h2>Tchalla Black Panther</h2>
-          </div>
-          <div className="profile__userInfo_bottom">
-            <div className="profile__userInfo_about">
-              <h2 className="profile__userInfo_about_header">About Me</h2>
-              <p className="profile__userInfo_about_content">
-                I am the king of wakanda and the current black panther. My
-                interests are bringing world peace.
-              </p>
+    <>
+      <EditProfileModal />
+      <div className="profile">
+        <div className="profile__userInfo-container">
+          <img src={avatar} alt="avatar" className="profile__userInfo_avatar" />
+          <div className="profile__userInfo">
+            <div className="profile__userInfo_top">
+              <h2>Tchalla Black Panther</h2>
             </div>
+            <div className="profile__userInfo_bottom">
+              <div className="profile__userInfo_about">
+                <h2 className="profile__userInfo_about_header">About Me</h2>
+                <p className="profile__userInfo_about_content">
+                  I am the king of wakanda and the current black panther. My
+                  interests are bringing world peace.
+                </p>
+              </div>
 
-            <ManageAccountsTwoToneIcon className="profile__editprofile-btn" />
+              <ManageAccountsTwoToneIcon className="profile__editprofile-btn" />
+            </div>
           </div>
         </div>
+        <div className="profile__tabs">
+          {tabs.map((tab) => {
+            return (
+              <h3
+                key={tab}
+                onClick={() => setActiveTab(tab)}
+                className={activeTab === tab ? "active" : "underline"}
+              >
+                {tab}
+              </h3>
+            );
+          })}
+        </div>
+        {activeTab === "Posts" ? <PostCard /> : null}
+        {activeTab === "Followers" ? <Followers /> : null}
+        {activeTab === "Following" ? <Following /> : null}
       </div>
-      <div className="profile__tabs">
-        {tabs.map((tab) => {
-          return (
-            <h3
-              key={tab}
-              onClick={() => setActiveTab(tab)}
-              className={activeTab === tab ? "active" : "underline"}
-            >
-              {tab}
-            </h3>
-          );
-        })}
-      </div>
-      {activeTab === "Posts" ? <PostCard /> : null}
-      {activeTab === "Followers" ? <Followers /> : null}
-      {activeTab === "Following" ? <Following /> : null}
-    </div>
+    </>
   );
 };
