16c9e77ce3c65260e86e5625623acbd9345695f1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-12-31 22:39:07 +0100

    add date

--- client/src/pages/Notification.tsx
+++ client/src/pages/Notification.tsx
@@ -1,4 +1,26 @@
 import "./Notification.css";
 export const Notification = () => {
-  return <div>Hello Notification</div>;
+  return (
+    <div>
+      {/* This component should show notifications
+    1. when you get followed
+    2. when someone likes your post
+    3. when someone comments on your post
+    Another thing is how do i clear a notification?
+    should i have a button? or when i open the page?
+     */}
+      <div>
+        <p> Micheal Scholfield followed you</p>
+        <p>2/12/2022</p>
+      </div>
+      <div>
+        <p> Ghandi liked your post</p>
+        <p>2/12/2022</p>
+      </div>
+      <div>
+        <p> Lincon added a comment to your post</p>
+        <p>2/12/2022</p>
+      </div>
+    </div>
+  );
 };
