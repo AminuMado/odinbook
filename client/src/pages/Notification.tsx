08547c96e7c2a9359c0f51aed7fc7864cc4dc641
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-01-03 17:34:52 +0100

    add links to notification

--- client/src/pages/Notification.tsx
+++ client/src/pages/Notification.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Notification.css";
 export const Notification = () => {
   const [isRead, setIsRead] = useState(true);
@@ -17,7 +18,12 @@ export const Notification = () => {
         } `}
       >
         <div className="notification__content">
-          <p> Micheal Scholfield followed you</p>
+          <p>
+            <Link to="/profile" className="notification__user">
+              Michael Scofield{" "}
+            </Link>
+            followed you
+          </p>
           <p>31/12/2022</p>
         </div>
         {isRead && (
@@ -33,7 +39,17 @@ export const Notification = () => {
         } `}
       >
         <div className="notification__content">
-          <p> Ghandi liked your post</p>
+          <p>
+            <Link to="/profile" className="notification__username">
+              {" "}
+              Ghandi
+            </Link>{" "}
+            liked your{" "}
+            <Link to="/postDetail" className="notification__post">
+              {" "}
+              post
+            </Link>
+          </p>
           <p>31/12/2022</p>
         </div>
         {isRead && (
@@ -49,7 +65,10 @@ export const Notification = () => {
         } `}
       >
         <div className="notification__content">
-          <p> Lincon added a comment to your post</p>
+          <p>
+            <Link to="/profile">Lincon</Link> added a comment to your{" "}
+            <Link to="/post">post</Link>
+          </p>
           <p>31/12/2022</p>
         </div>
         {isRead && (
