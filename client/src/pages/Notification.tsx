ee6be32e4884f75a8c433929c1732805701e6ac7
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-01-09 16:09:36 +0100

    add id props and update display logic

--- client/src/pages/Notification.tsx
+++ client/src/pages/Notification.tsx
@@ -1,83 +1,59 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Notification.css";
-export const Notification = () => {
-  const [isRead, setIsRead] = useState(true);
+import { useUpdateNotifcation } from "../hooks/useUpdateNotification";
+type props = {
+  id: string;
+  profileName: string;
+  profileId: string;
+  date: string;
+  postId: string | null;
+  type: string;
+  isViewed: boolean;
+};
+export const Notification = ({
+  id,
+  profileName,
+  profileId,
+  date,
+  postId,
+  type,
+  isViewed,
+}: props) => {
+  const [isRead, setIsRead] = useState(isViewed);
+  const { update } = useUpdateNotifcation();
+  let placeholder = "followed you";
+  if (type === "comment") {
+    placeholder = "added a comment to your ";
+  }
+  if (type === "like") {
+    placeholder = "liked your ";
+  }
   return (
-    <div>
-      {/* This component should show notifications
-    1. when you get followed
-    2. when someone likes your post
-    3. when someone comments on your post
-    Another thing is how do i clear a notification?
-    should i have a button? or when i open the page?
-     */}
-      <div
-        className={`notification-container ${
-          isRead ? "notification-unread" : "notification-read"
-        } `}
-      >
-        <div className="notification__content">
-          <p>
-            <Link to="/profile" className="notification__user">
-              Michael Scofield{" "}
-            </Link>
-            followed you
-          </p>
-          <p>31/12/2022</p>
-        </div>
-        {isRead && (
-          <div
-            className="notification__markAsRead"
-            onClick={() => setIsRead(false)}
-          />
-        )}
+    <div
+      className={`notification-container ${
+        isRead ? "notification-read" : "notification-unread"
+      } `}
+    >
+      <div className="notification__content">
+        <Link to={`${profileId}/profile`} className="notification__user">
+          {profileName}
+        </Link>
+        <p>
+          {placeholder}{" "}
+          {postId && <Link to={`${postId}/postDetails`}>post</Link>}
+        </p>
+        <p>{date}</p>
       </div>
-      <div
-        className={`notification-container ${
-          isRead ? "notification-unread" : ""
-        } `}
-      >
-        <div className="notification__content">
-          <p>
-            <Link to="/profile" className="notification__username">
-              {" "}
-              Ghandi
-            </Link>{" "}
-            liked your{" "}
-            <Link to="/postDetail" className="notification__post">
-              {" "}
-              post
-            </Link>
-          </p>
-          <p>31/12/2022</p>
-        </div>
-        {isRead && (
-          <div
-            className="notification__markAsRead"
-            onClick={() => setIsRead(false)}
-          />
-        )}
-      </div>
-      <div
-        className={`notification-container ${
-          isRead ? "notification-unread" : ""
-        } `}
-      >
-        <div className="notification__content">
-          <p>
-            <Link to="/profile">Lincon</Link> added a comment to your{" "}
-            <Link to="/post">post</Link>
-          </p>
-          <p>31/12/2022</p>
-        </div>
-        {isRead && (
-          <div
-            className="notification__markAsRead"
-            onClick={() => setIsRead(false)}
-          />
-        )}
-      </div>
+      {!isRead && (
+        <div
+          className="notification__markAsRead"
+          onClick={() => {
+            update(id);
+            setIsRead(true);
+          }}
+        />
+      )}
     </div>
   );
 };
