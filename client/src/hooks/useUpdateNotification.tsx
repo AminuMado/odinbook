bb0ff00de7d48e63f2da38f1a0c052fb5f5982d3
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-01-09 21:05:09 +0100

    add an update all method

--- client/src/hooks/useUpdateNotification.tsx
+++ client/src/hooks/useUpdateNotification.tsx
@@ -2,33 +2,40 @@ import { useUserContext } from "./useUserContext";
 
 export const useUpdateNotifcation = () => {
   const { state, dispatch } = useUserContext();
-  const url = `https://jsonplaceholder.typicode.com/users/1`;
-  const update = async (id: string) => {
-    const response = await fetch(url); // this should be a post request
-    /*
-        const response = await fetch(url, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({id}),
-        });
-    */
-    // you should get the id of the notification and just update it
-    // furthermore if i can manage it i should have an update all for a user in the backend
-    const json = await response.json();
-    if (!response.ok) {
-      return;
-    }
-    if (response.ok) {
-      // to update you access the notifications and change the state we can do this using the filter method
-      const notifications = state.user.notifications;
-      notifications.forEach((notification) => {
-        if (notification.id === id) {
-          notification.isSeen = true;
-        }
-      });
-      dispatch({ type: "UPDATE_NOTIFICATION", payload: notifications });
-      // update the loading state
-    }
+  const updateAll = () => {
+    // just have the backend url here
+    const notifications = state.user.notifications;
+    notifications.forEach((notification) => (notification.isSeen = true));
+    dispatch({ type: "UPDATE_NOTIFICATION", payload: notifications });
   };
-  return { update };
+  return { updateAll };
 };
+
+//   const url = `https://jsonplaceholder.typicode.com/users/1`;
+//   const update = async (id: string) => {
+//     const response = await fetch(url); // this should be a post request
+//     /*
+//         const response = await fetch(url, {
+//             method: "POST",
+//             headers: { "Content-Type": "application/json" },
+//             body: JSON.stringify({id}),
+//         });
+//     */
+//     // you should get the id of the notification and just update it
+//     // furthermore if i can manage it i should have an update all for a user in the backend
+//     const json = await response.json();
+//     if (!response.ok) {
+//       return;
+//     }
+//     if (response.ok) {
+//       // to update you access the notifications and change the state we can do this using the filter method
+//       const notifications = state.user.notifications;
+//       notifications.forEach((notification) => {
+//         if (notification.id === id) {
+//           notification.isSeen = true;
+//         }
+//       });
+//       dispatch({ type: "UPDATE_NOTIFICATION", payload: notifications });
+//       // update the loading state
+//     }
+//   };
