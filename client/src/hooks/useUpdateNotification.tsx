caefa62925abb638e145dc0bc4eb93f1178ffb34
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-01-09 16:10:00 +0100

    remove unused code

--- client/src/hooks/useUpdateNotification.tsx
+++ client/src/hooks/useUpdateNotification.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
 import { useUserContext } from "./useUserContext";
 
 export const useUpdateNotifcation = () => {
-  const [error, setError] = useState<null | string>(null);
-  const [isLoading, setIsLoading] = useState(false);
   const { state, dispatch } = useUserContext();
   const url = `https://jsonplaceholder.typicode.com/users/1`;
   const update = async (id: string) => {
-    setIsLoading(true);
-    setError(null);
     const response = await fetch(url); // this should be a post request
     /*
         const response = await fetch(url, {
@@ -21,8 +16,7 @@ export const useUpdateNotifcation = () => {
     // furthermore if i can manage it i should have an update all for a user in the backend
     const json = await response.json();
     if (!response.ok) {
-      setIsLoading(false);
-      setError(json.error);
+      return;
     }
     if (response.ok) {
       // to update you access the notifications and change the state we can do this using the filter method
@@ -34,9 +28,7 @@ export const useUpdateNotifcation = () => {
       });
       dispatch({ type: "UPDATE_NOTIFICATION", payload: notifications });
       // update the loading state
-      setIsLoading(false);
-      console.log(notifications);
     }
   };
-  return { update, isLoading, error };
+  return { update };
 };
