ce245f87b8707b6b65285d4ddc64f9fcade7fe6e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-01-27 20:56:56 +0100

    import explore component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { useAuthContext } from "./hooks/useAuthContext";
 import { Profile } from "./pages/Profile";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Notifications } from "./pages/Notifications";
+import { Explore } from "./pages/Explore";
 function App() {
   const { state } = useAuthContext();
   const user = state.user;
@@ -19,7 +20,6 @@ function App() {
             path="/"
             element={!user ? <Landing /> : <Navigate to="/home" />}
           />
-
           <Route path="/home" element={user ? <Home /> : <Navigate to="/" />} />
           <Route
             path="/profile"
@@ -28,6 +28,10 @@ function App() {
           <Route
             path="/notification"
             element={user ? <Notifications /> : <Navigate to="/" />}
+          />{" "}
+          <Route
+            path="/explore"
+            element={user ? <Explore /> : <Navigate to="/" />}
           />
         </Routes>
       </div>
