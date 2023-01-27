298aca899f9479a97d6964cce4955abced02f7bf
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-01-27 21:01:12 +0100

    add link to css file

--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -1,3 +1,8 @@
+import "./Explore.css";
 export const Explore = () => {
+  // So we want to have a all users mode. So you can see all active users.
+  // Most liked Posts
+  // Most commented Posts
+  // Personal Message of how the site works or a message from me to the user with some gifs etc
   return <div>Hello Explore</div>;
 };
