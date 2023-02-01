ce2fa060a066ee31de55958e082509c1e0ee77ae
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-02-01 23:18:13 +0100

    add placeholders

--- client/src/components/Explore/ExplorePosts.tsx
+++ client/src/components/Explore/ExplorePosts.tsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
 import "./ExplorePosts.css";
 export const ExplorePosts = () => {
-  return <div>Hello Posts</div>;
+  const [activeTab, setActiveTab] = useState("");
+  const tabs = ["Date", "Comments", "Likes"]; // you have to update me
+
+  return (
+    <div>
+      {/* A tab containg the sorting icon and the list of things to be done to the page */}
+      {/* A container that has the loading animation until a fetch is done if it fails there should be an error message or a gif depicting something went wrong */}
+      {/* Since its a post page should i have a summary post place holder so that if you wanna engage you gotta to to the post details page. or just have the post itself */}
+      <nav className="explorePosts-nav">
+        <ul>
+          {tabs.map((tab) => {
+            return <li onClick={() => setActiveTab(tab)}>{tab}</li>;
+          })}
+        </ul>
+      </nav>
+      <div className="explorePosts-details"></div>
+    </div>
+  );
 };
--- client/src/components/Explore/ExploreUsers.tsx
+++ client/src/components/Explore/ExploreUsers.tsx
@@ -1,4 +1,11 @@
 import "./ExploreUsers.css";
 export const ExploreUsers = () => {
-  return <div>Hello ExploreUsers</div>;
+  return (
+    <div>
+      {/* A tab containing the various sort categories */}
+      {/* A container containg the users in their various sort modes all users(in alpabeticall order), the OGs (first to join), new to join,most followers, most following */}
+      {/* A loading icon when a call to the db is made no saving state locally */}
+      Hello ExploreUsers
+    </div>
+  );
 };
