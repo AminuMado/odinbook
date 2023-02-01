14745b4b54318c516bcfb158005334b040475647
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-02-01 23:17:54 +0100

    add quote

--- client/src/components/Explore/ExploreMessage.tsx
+++ client/src/components/Explore/ExploreMessage.tsx
@@ -1,4 +1,14 @@
 import "./ExploreMessage.css";
 export const ExploreMessage = () => {
-  return <div>Hello ExploreMssg</div>;
+  return (
+    <div className="quote-container">
+      <blockquote className="quote">
+        I am built upon the small things I do everyday, and the end results are
+        no more than a byproduct of that.
+        <span className="quotemark_left">❝</span>
+        <span className="quotemark_right">❞</span>
+      </blockquote>
+      <small className="quote_author">Shinsuke Kita</small>
+    </div>
+  );
 };
