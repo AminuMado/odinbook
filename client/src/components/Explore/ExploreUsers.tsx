7bdbf09daee21356a4c1cd9055171ab4d9e48e89
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-02-02 12:33:47 +0100

    add placeholder tabs

--- client/src/components/Explore/ExploreUsers.tsx
+++ client/src/components/Explore/ExploreUsers.tsx
@@ -1,11 +1,16 @@
 import "./ExploreUsers.css";
 export const ExploreUsers = () => {
   return (
-    <div>
+    <div className="exploreUsers-container">
       {/* A tab containing the various sort categories */}
       {/* A container containg the users in their various sort modes all users(in alpabeticall order), the OGs (first to join), new to join,most followers, most following */}
       {/* A loading icon when a call to the db is made no saving state locally */}
-      Hello ExploreUsers
+      <div className="exploreUsers__tab">
+        {/* The explore user sortable tab goes here */}
+      </div>
+      <div className="exploreUsers__details">
+        {/* The explore user card goes here */}
+      </div>
     </div>
   );
 };
