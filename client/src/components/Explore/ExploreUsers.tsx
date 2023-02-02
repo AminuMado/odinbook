dc27cb53d977209e287cfae0c2a6603497d16f34
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-02-02 22:08:51 +0100

    add tabs

--- client/src/components/Explore/ExploreUsers.tsx
+++ client/src/components/Explore/ExploreUsers.tsx
@@ -1,12 +1,28 @@
 import "./ExploreUsers.css";
+import { useState } from "react";
 export const ExploreUsers = () => {
+  const [activeTab, setActiveTab] = useState("Date");
+  const tabs = ["Date", "Followers", "Following"];
+
   return (
     <div className="exploreUsers-container">
       {/* A tab containing the various sort categories */}
       {/* A container containg the users in their various sort modes all users(in alpabeticall order), the OGs (first to join), new to join,most followers, most following */}
       {/* A loading icon when a call to the db is made no saving state locally */}
       <div className="exploreUsers__tab">
-        {/* The explore user sortable tab goes here */}
+        {tabs.map((tab) => {
+          return (
+            <li
+              key={tab}
+              className={activeTab === tab ? "active" : ""}
+              onClick={() => {
+                setActiveTab(tab);
+              }}
+            >
+              {tab}
+            </li>
+          );
+        })}
       </div>
       <div className="exploreUsers__details">
         {/* The explore user card goes here */}
