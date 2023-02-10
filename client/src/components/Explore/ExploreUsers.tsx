4f770fa82cd902ae09f8a900c1fb2e46a1c045a3
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-02-10 22:51:26 +0100

    added logic

--- client/src/components/Explore/ExploreUsers.tsx
+++ client/src/components/Explore/ExploreUsers.tsx
@@ -2,22 +2,37 @@ import "./ExploreUsers.css";
 import { useState } from "react";
 import { ExploreUserCard } from "./ExploreUserCard";
 export const ExploreUsers = () => {
-  const [activeTab, setActiveTab] = useState("Date");
+  const [activeTab, setActiveTab] = useState({ tab: "date", sorting: "desc" });
   const tabs = ["Date", "Followers", "Following"];
-
+  const handleClick = (tab: string) => {
+    // first condition is to check if the current tab is clicked
+    // if thats the case we just wanna update the sorting;
+    // if its a different tab then we wanna update the tab and switch the sorting to the default
+    if (tab === activeTab.tab) {
+      const nextSorting = activeTab.sorting === "desc" ? "asc" : "desc";
+      const update = { tab: tab, sorting: nextSorting };
+      setActiveTab(update);
+    } else {
+      const update = { tab: tab, sorting: "desc" };
+      setActiveTab(update);
+    }
+  };
   return (
     <div className="exploreUsers-container">
       {/* A tab containing the various sort categories */}
       {/* A container containg the users in their various sort modes all users(in alpabeticall order), the OGs (first to join), new to join,most followers, most following */}
       {/* A loading icon when a call to the db is made no saving state locally */}
+      {/* I want to have  */}
       <div className="exploreUsers__tab">
         {tabs.map((tab) => {
           return (
             <li
               key={tab}
-              className={activeTab === tab ? "active" : ""}
+              className={
+                activeTab.tab === tab ? "active " + activeTab.sorting : ""
+              }
               onClick={() => {
-                setActiveTab(tab);
+                handleClick(tab);
               }}
             >
               {tab}
