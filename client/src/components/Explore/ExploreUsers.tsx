cc16afc0f015e13ffa66809f5027376e70d6e5e5
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-02-20 22:08:57 +0100

    import usesort hook and add logic.... Still in progess

--- client/src/components/Explore/ExploreUsers.tsx
+++ client/src/components/Explore/ExploreUsers.tsx
@@ -1,7 +1,10 @@
 import "./ExploreUsers.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ExploreUserCard } from "./ExploreUserCard";
+import { useSort } from "../../hooks/useSort";
 export const ExploreUsers = () => {
+  const [users, setUsers] = useState(null);
+  const { sort, isLoading, error } = useSort();
   const [activeTab, setActiveTab] = useState({ tab: "Date", sorting: "desc" });
   const tabs = ["Date", "Followers", "Following"];
   const handleClick = (tab: string) => {
@@ -17,12 +20,18 @@ export const ExploreUsers = () => {
       setActiveTab(update);
     }
   };
+  useEffect(() => {
+    // This would have to be reworked on after i rig up the backend
+    const usersFromDB = sort(activeTab.tab, activeTab.sorting);
+    // setUserss(usersFromDB);
+  }, [activeTab]);
   return (
     <div className="exploreUsers-container">
       {/* A tab containing the various sort categories */}
       {/* A container containg the users in their various sort modes all users(in alpabeticall order), the OGs (first to join), new to join,most followers, most following */}
       {/* A loading icon when a call to the db is made no saving state locally */}
       {/* I want to have  */}
+      {/* Gotta import the loader to alternate */}
       <div className="exploreUsers__tab">
         {tabs.map((tab) => {
           return (
@@ -41,6 +50,7 @@ export const ExploreUsers = () => {
         })}
       </div>
       <div className="exploreUsers__details">
+        {/* check if is Loading then show the loading animation else do the whole mapping thing */}
         {/* The explore user card goes here */}
         <ExploreUserCard />
         <ExploreUserCard />
