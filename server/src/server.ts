3db7b1bc81310720281ff3e8102ac9c218975366
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-03-05 11:15:02 +0100

    add routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Application, NextFunction } from "express";
 import { Server } from "http";
 import { config } from "dotenv";
+import postRoutes from "./routes/posts";
 // Loads .env file contents into process.env.
 config();
 
@@ -10,8 +11,10 @@ const port: Number = Number(process.env.PORT);
 const server: Server = app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
-
+// Routes
+app.use("/api/posts", postRoutes);
 // Index page at default entry route
 app.route("/").get((req: Request, res: Response) => {
-  res.sendFile(process.cwd() + "/index.html");
+  console.log(process.cwd());
+  res.sendFile(process.cwd() + "/src/index.html");
 });
