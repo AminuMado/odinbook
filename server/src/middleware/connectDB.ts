39eb0de60ab46cd3cb4fd2bdadb8931e3c27d5da
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-03-07 00:26:40 +0100

    initial commit

---
+++ server/src/middleware/connectDB.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import { type } from "os";
+// Loads .env file contents into process.env.
+config();
+
+const DB_STRING = String(process.env.MONGODB_URI);
+
+/**
+ * -------------- DATABASE ----------------
+ */
+
+/**
+ * Connect to MongoDB Server using the connection string in the `.env` file.
+ * To implement this, place the following string into the `.env` file
+ * MONGODB_URI=mongodb://<user>:<password>@localhost:27017/database_name
+ */
+
+export const connectDB = async (cb: () => void) => {
+  try {
+    const db = await mongoose.connect(DB_STRING);
+    console.log(`MongoDB Connected: ${db.connection.host}`);
+    cb();
+  } catch (error) {
+    const db = mongoose.connection;
+    //Bind connection to error event (to get notification of connection errors)
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  }
+};
