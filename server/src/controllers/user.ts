9de79dabb8dd88ae000de1e4c26df76931d74e41
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-05-01 19:41:37 +0100

    initial commit

---
+++ server/src/controllers/user.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import mongoose, { Error } from "mongoose";
+import User from "../models/user";
+
+/* --------- Log In --------- */
+
+const loginUser = async (req: Request, res: Response) => {
+  // Get the username email and password from the request
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const username = user.username;
+
+    // create a token
+    // const token = createToken(user._id);
+
+    // respond with the token and username
+    // res.status(200).json({ username, token });
+    res.status(200).json({ user });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/* --------- Sign Up --------- */
+
+const signupUser = async (req: Request, res: Response) => {
+  // Get the username email and password from the request
+  const { username, email, password } = req.body;
+  try {
+    const user = await User.signup(username, email, password);
+
+    // Create a Token
+    // const token = createToken(user._id);
+
+    // Respond with the username and token
+    // res.status(200).json({ username, token });
+    res.status(201).json({ user });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export default { loginUser, signupUser };
