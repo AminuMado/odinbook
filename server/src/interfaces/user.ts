178c21127f4d63fea7acb1060a6b37a198005a64
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-05-01 19:41:23 +0100

    initial commit

---
+++ server/src/interfaces/user.ts
@@ -0,0 +1,24 @@
+import { Document, Model } from "mongoose";
+
+export interface IUser extends Document {
+  // We would have to make a better choice betweeen null or opional in fields;
+  // Would have to address the object[] thingy would need it to be another model eg <comments>[]
+  username: string;
+  name: string;
+  password: string;
+  about: string;
+  email: string;
+  avatar: string | null;
+  followers: object[];
+  following: object[];
+  posts: object[];
+  comments: object[];
+  notifications: object[];
+  isAdmin: boolean;
+  images: string[];
+}
+
+export interface UserModel extends Model<IUser> {
+  login(email: string, password: string): Promise<IUser>;
+  signup(username: string, email: string, password: string): Promise<IUser>;
+}
