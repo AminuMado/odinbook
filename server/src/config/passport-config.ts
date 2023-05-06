fb63051700b7203c80f6343b2dd988c3fbd5c3ef
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-05-06 22:58:54 +0100

    add user login and creation

--- server/src/config/passport-config.ts
+++ server/src/config/passport-config.ts
@@ -12,9 +12,25 @@ passport.use(
       clientSecret: config.google.clientSecret,
       callbackURL: "/auth/google/redirect",
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       // get profile details
       // save profile details in db
+      const user = await User.findOne({ googleId: profile.id });
+
+      // If user doesn't exist creates a new user. (similar to sign up)
+      if (!user) {
+        const newUser = await User.create({
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails?.[0].value,
+          // we are using optional chaining because profile.emails may be undefined.
+        });
+        if (newUser) {
+          done(null, newUser);
+        }
+      } else {
+        done(null, user);
+      }
     }
   )
 );
