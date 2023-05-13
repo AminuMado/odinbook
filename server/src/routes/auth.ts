29d89ad01cf01d0e64cb4f9e45efb8fd3b4a7818
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-05-13 23:10:13 +0100

    add logOut route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -11,5 +11,10 @@ router.get(
 router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   res.send("This is the callback route");
 });
+router.get("/logout", function (req, res, next) {
+  // @ts-ignore
+  req.logOut();
+  res.status(200).send("Logged Out");
+});
 
 export = router;
