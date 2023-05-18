76280a2a9d0934f0f64353b45b9f90b78dfa3377
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-05-18 23:02:48 +0100

    add google signup button

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LoginForm } from "../components/LoginForm";
 import { SignupForm } from "../components/SignupForm";
+import GoogleIcon from "@mui/icons-material/Google";
 import "./Landing.css";
 export const Landing = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -17,6 +18,10 @@ export const Landing = () => {
         <h1 className="Landing__testaccount">Just looking around?</h1>
         <Link to="/home">
           <button className="Landing__testaccountBtn">Test Account</button>
+          <button className="Landing__googleSignupBtn">
+            <GoogleIcon />
+            Google Sign Up
+          </button>
         </Link>
       </div>
       <div className="Landing__btn-container">
