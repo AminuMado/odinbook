0886cae0e81469540d746cb47e8e61fd32ed8376
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-05-25 14:59:02 +0100

    add window open for google auth sign in in the backend

--- client/src/hooks/useGoogleSignIn.tsx
+++ client/src/hooks/useGoogleSignIn.tsx
@@ -4,16 +4,17 @@ import { useAuthContext } from "./useAuthContext";
 export const useGoogleSignIn = () => {
   const [error, setError] = useState<null | string>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const url = "/auth/google";
+  const url = "http://localhost:4000/auth/google";
   const { state, dispatch } = useAuthContext();
   console.log(state.user);
   const login = async () => {
     setIsLoading(true);
     setError(null);
-    const response = await fetch(url, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
+    window.open(url, "_self");
+    // const response = await fetch(url, {
+    //   method: "GET",
+    //   headers: { "Content-Type": "application/json" },
+    // });
     // const json = await response.json();
     // if (!response.ok) {
     //   setIsLoading(false);
