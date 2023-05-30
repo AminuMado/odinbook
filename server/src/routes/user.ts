ec4e7e174ae17444b238aefbb33a5673141d4941
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-05-30 15:39:13 +0100

    add getUser route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -2,7 +2,7 @@ import express from "express";
 import controller from "../controllers/user";
 
 const router = express.Router();
-
+router.get("/getUser", controller.getUser);
 router.post("/create/user", controller.signupUser);
 router.post("/login/user", controller.loginUser);
 
