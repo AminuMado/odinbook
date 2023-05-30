71f0c3c9a8a12ba1b26fc27be691a74425a7a3bb
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-05-30 15:26:20 +0100

    add get user method

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+/* --------- Get User --------- */
+const getUser = (req: Request, res: Response) => {
+  const user = req.user;
+  if (!user) {
+    res.status(404).json({ error: "No User Signed In" });
+  }
+  res.status(200).json({ user });
+};
 /* --------- Log In --------- */
 
 const loginUser = async (req: Request, res: Response) => {
@@ -40,4 +48,4 @@ const signupUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { loginUser, signupUser };
+export default { getUser, loginUser, signupUser };
