6e1ea6758586c918f30da9ad11573482899f639f
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-06-03 00:08:06 +0100

    update function to have a single responsibility

--- client/src/hooks/useGoogleSignIn.tsx
+++ client/src/hooks/useGoogleSignIn.tsx
@@ -11,25 +11,6 @@ export const useGoogleSignIn = () => {
     setIsLoading(true);
     setError(null);
     window.open(url, "_self");
-    // const response = await fetch(url, {
-    //   method: "GET",
-    //   headers: { "Content-Type": "application/json" },
-    // });
-    // const json = await response.json();
-    // if (!response.ok) {
-    //   setIsLoading(false);
-    //   setError(json.error);
-    // }
-    // if (response.ok) {
-    //   console.log(JSON.stringify(json));
-    // save the user to local storage
-    // localStorage.setItem("user", JSON.stringify(json));
-    // update the auth context
-    //  const user = { username: "Aminu Mukhtar", token: "12345678" };
-    //  dispatch({ type: "LOGIN", payload: user });
-    // update the loading state
-    //   setIsLoading(false);
-    // }
   };
   return { login, isLoading, error };
 };
