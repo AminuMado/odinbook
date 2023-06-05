1b862a05a4380cc1e288325137e7999d51b734dc
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-06-05 13:03:10 +0100

    comment out unused code

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import User from "../models/user";
+// import User from "../models/user";
 
 /* --------- Get User --------- */
 const getUser = (req: Request, res: Response) => {
@@ -10,43 +10,43 @@ const getUser = (req: Request, res: Response) => {
     res.status(200).json({ user });
   }
 };
-/* --------- Log In --------- */
+// /* --------- Log In --------- */
 
-const loginUser = async (req: Request, res: Response) => {
-  // Get the username email and password from the request
-  const { email, password } = req.body;
-  try {
-    const user = await User.login(email, password);
-    const username = user.username;
+// const loginUser = async (req: Request, res: Response) => {
+//   // Get the username email and password from the request
+//   const { email, password } = req.body;
+//   try {
+//     const user = await User.login(email, password);
+//     const username = user.username;
 
-    // create a token
-    // const token = createToken(user._id);
+//     // create a token
+//     // const token = createToken(user._id);
 
-    // respond with the token and username
-    // res.status(200).json({ username, token });
-    res.status(200).json({ user });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
-};
+//     // respond with the token and username
+//     // res.status(200).json({ username, token });
+//     res.status(200).json({ user });
+//   } catch (error: any) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
-/* --------- Sign Up --------- */
+// /* --------- Sign Up --------- */
 
-const signupUser = async (req: Request, res: Response) => {
-  // Get the username email and password from the request
-  const { username, email, password } = req.body;
-  try {
-    const user = await User.signup(username, email, password);
+// const signupUser = async (req: Request, res: Response) => {
+//   // Get the username email and password from the request
+//   const { username, email, password } = req.body;
+//   try {
+//     const user = await User.signup(username, email, password);
 
-    // Create a Token
-    // const token = createToken(user._id);
+//     // Create a Token
+//     // const token = createToken(user._id);
 
-    // Respond with the username and token
-    // res.status(200).json({ username, token });
-    res.status(201).json({ user });
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
-};
+//     // Respond with the username and token
+//     // res.status(200).json({ username, token });
+//     res.status(201).json({ user });
+//   } catch (error: any) {
+//     res.status(400).json({ error: error.message });
+//   }
+// };
 
-export default { getUser, loginUser, signupUser };
+export default { getUser };
